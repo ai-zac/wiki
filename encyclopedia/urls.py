72fa860618892598af8d307eb36d00779a9e0447
ai-zac <isaacjdv31@gmail.com>
2023-10-24 16:39:36 -0600

    Finish: 'New page', 'Edit page' & 'Random page' requirements

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("create/", views.create_entry, name="create"),
+    path("random/", views.random_entry, name="random"),
+    path("search/", views.search, name="search"),
     path("wiki/<str:title>/", views.entry_preview, name="entry"),
-    path("wiki/search/", views.search, name="search"),
-    path("wiki/create/", , name="create")
+    path("edit/<str:title>/", views.edit_entry, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,15 +1,18 @@
+from random import choice
 from django.http import HttpResponseNotFound
 from django.shortcuts import redirect, render
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_http_methods
 from markdown2 import markdown
 
 from . import util
 
 
+@require_GET
 def index(request):
     return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
 
+@require_GET
 def entry_preview(request, title):
     entry = util.get_entry(title)
     if entry is None:
@@ -17,14 +20,15 @@ def entry_preview(request, title):
         body = render(request, "encyclopedia/error.html", context)
         return HttpResponseNotFound(body)
 
-    context = {"title": title, "content": markdown(entry)}
-    return render(request, "encyclopedia/entry.html", context)
+    return render(
+        request, "encyclopedia/entry.html", {"title": title, "content": markdown(entry)}
+    )
 
 
 @require_GET
 def search(request):
-    r = request.GET.copy()
-    q = r["q"]
+    p = request.GET.copy()
+    q = p["q"]
     entries = util.list_entries()
     s = [m for m in entries if q in m]
 
@@ -33,5 +37,38 @@ def search(request):
     return render(request, "encyclopedia/index.html", {"entries": s})
 
 
+@require_http_methods(["GET", "POST"])
 def create_entry(request):
-    return render(request, "encyclopedia/create.html", {})
+    if request.method == "GET":
+        return render(request, "encyclopedia/create.html")
+    else:
+        r = request.POST.copy()
+        entries = util.list_entries()
+        if r["title"] in entries:
+            context = {"code": 400, "description": "Bad request, entry already exists"}
+            body = render(request, "encyclopedia/error.html", context)
+            return HttpResponseNotFound(body)
+
+        util.save_entry(r["title"], r["content"])
+        return redirect("entry", title=r["title"])
+
+
+@require_http_methods(["GET", "POST"])
+def edit_entry(request, title):
+    if request.method == "GET":
+        entry = util.get_entry(title)
+
+        return render(
+            request, "encyclopedia/edit.html", {"title": title, "body": entry}
+        )
+    else:
+        r = request.POST.copy()
+        util.save_entry(title, r["content"])
+    return redirect("entry", title=title)
+
+
+@require_GET
+def random_entry(request):
+    entries = util.list_entries()
+    entry = choice(entries)
+    return redirect("entry", title=entry)
