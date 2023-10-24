df38e853858a684d64d59a3e8ce518e5a3d8528a
ai-zac <isaacjdv31@gmail.com>
2023-10-24 14:48:46 -0600

    Finish: 'Entry page', 'Index page', 'Search' requirements

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:title>/", views.entry_preview, name="entry"),
+    path("wiki/search/", views.search, name="search"),
+    path("wiki/create/", , name="create")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,37 @@
-from django.shortcuts import render
+from django.http import HttpResponseNotFound
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_GET
+from markdown2 import markdown
 
 from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html", {"entries": util.list_entries()})
 
+
+def entry_preview(request, title):
+    entry = util.get_entry(title)
+    if entry is None:
+        context = {"code": 404, "description": "Entry not found"}
+        body = render(request, "encyclopedia/error.html", context)
+        return HttpResponseNotFound(body)
+
+    context = {"title": title, "content": markdown(entry)}
+    return render(request, "encyclopedia/entry.html", context)
+
+
+@require_GET
+def search(request):
+    r = request.GET.copy()
+    q = r["q"]
+    entries = util.list_entries()
+    s = [m for m in entries if q in m]
+
+    if len(s):
+        return redirect("entry", title=s[0])
+    return render(request, "encyclopedia/index.html", {"entries": s})
+
+
+def create_entry(request):
+    return render(request, "encyclopedia/create.html", {})
